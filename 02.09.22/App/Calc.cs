71ca61710fcc5e7b911344af6ecb70febacdd863
Lin <Lin@DESKTOP-EOF2L9K>
2022-09-07 16:52:08 +0300

    hj

--- 02.09.22/App/Calc.cs
+++ 02.09.22/App/Calc.cs
@@ -10,7 +10,7 @@ namespace CalcProject.App
     {
         public void Run()
         {
-            Console.WriteLine("cvadrocopter");
+            Console.WriteLine("copter");
             Console.WriteLine("Priv");
         }
     }
